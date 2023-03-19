65195db3d5fb222945285ec01e64f9a002eae0a2
Lyka <leimicah.b9090@gmail.com>
2023-03-20 10:32:19 +1300

    adding questions

---
+++ Quiz2.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from tkinter import  StringVar
+
+root = tk.Tk()
+root.geometry("500x500")
+
+questions = ["1 + 1 = ?","2 + 1 = ?""3 + 1 = ?""4 + 1 = ?""5 + 1 = ?"]
+options = [['A','B','2','1'],['A','3','2','1'],['A','B','2','4'],['5','B','2','1'],['A','6','2','1']]
